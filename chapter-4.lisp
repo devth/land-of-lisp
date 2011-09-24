32812bd22e6792059c5fbce670f763c9e2c36606
Trevor Hartman <trevorhartman@gmail.com>
2011-09-24 12:23:17 -0700

    Finish Chapter 4

--- chapter-4.lisp
+++ chapter-4.lisp
@@ -56,6 +56,52 @@
 (text-editor 'vim)
 (text-editor 'textmate)
 
-
 ; conditionals tricks
 (and (oddp 5) (oddp 7) (oddp 9))
+
+; member returns truthy
+(if (member 0 '(1 2 3 4))
+        'is-in-the-list
+        'is-not-in-the-list)
+
+; even a list that only contains `nil` is truthy
+(if '(nil)
+  'true
+  'false)
+
+; benefiting from rich return values + high order functions
+(find-if #'oddp '(2 4 5 6)) ; returns 5
+
+(find-if #'oddp '(2 4 6)) ; returns nil
+
+(if (find-if #'oddp '(2 4 5 9))
+  'there-is-an-odd-number
+  'nope-no-odd-number)
+
+
+;
+; an overview of CL's comparison mechanisms
+; 
+
+; use `eq` to compare symbols
+(eq 'apple 'apple)
+
+(defparameter *orange* 'orange)
+(eq *orange* 'orange)
+
+; use 'equal` for everything else - isomorphic comparison, i.e. "they look the same"
+(equal 'apple 'apple)
+(equal 1.0 1) ; nil
+(eq 1.0 1) ; nil
+(equal 2 2)
+
+; use 'eql' for symbols, numbers and characters
+(eql 'foo 'foo)
+(eql 3.4 3.4)
+
+; 'equalp` is like `equal` except it can handle difficult comparison cases with
+; extra sophistication (strings w/ different capitalizations and integers against
+; floating point numbers)
+(equalp 1 1.0) ; T
+(equalp "Jim Jones" "jim jones") ; T
+
